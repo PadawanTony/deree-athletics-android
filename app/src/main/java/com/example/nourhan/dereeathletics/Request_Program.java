70dcd268511290b2bb329e2aa8082492c19d9d2c
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-11-28 23:15:55 +0200

    Create basic UI for request_program.xml

--- app/src/main/java/com/example/nourhan/dereeathletics/Request_Program.java
+++ app/src/main/java/com/example/nourhan/dereeathletics/Request_Program.java
@@ -1,9 +1,15 @@
 package com.example.nourhan.dereeathletics;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Nourhan on 11/25/16.
  */
 public class Request_Program extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.request_program);
+    }
 }
